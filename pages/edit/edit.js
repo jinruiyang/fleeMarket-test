42f929c070efccd11f880f3142b01b946b6f5624
jinrui yang <jinrui.yang@hotmail.com>
2018-06-01 15:56:58 +0800

    finish fix preview bug in edit

--- pages/edit/edit.js
+++ pages/edit/edit.js
@@ -542,7 +542,7 @@ Page({
   previewImage: function (e) {
     wx.previewImage({
       current: e.currentTarget.id, // 当前显示图片的http链接
-      urls: this.data.files // 需要预览的图片http链接列表
+      urls: this.data.page_images // 需要预览的图片http链接列表
     })
   },
   /**删除图片 */
